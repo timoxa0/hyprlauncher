bb35f015d3e2be8e5602af3aeac950244ebb962d
flk <f_l_k@t-online.de>
2024-10-28 23:22:36 +0100

    fix: application_id naming (#19)
    * fix: application_id naming
    
    Signed-off-by: Markus Volk <f_l_k@t-online.de>
    
    * fix the formatting
    
    ---------
    
    Signed-off-by: Markus Volk <f_l_k@t-online.de>
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>

--- src/app.rs
+++ src/app.rs
@@ -9,7 +9,7 @@ pub struct App {
 impl App {
     pub fn new() -> Self {
         let app = Application::builder()
-            .application_id("nnyyxxxx.hyprlauncher")
+            .application_id("hyprlauncher")
             .build();
 
         Self { app }
